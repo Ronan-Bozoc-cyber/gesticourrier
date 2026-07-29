ac0bb51efac6e3c0cc1617655b10d02093b79117
Ronan Bozoc <ronan@kali-robo>
2026-07-29 14:19:10 +0200

    Enhance security: Use .env for database credentials

--- partials/connexion.php
+++ partials/connexion.php
@@ -1,8 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "utilisateur";
-$password = "mdp1234";
-$dbname = "courriers_db";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
+// Désactiver les exceptions mysqli automatiques (PHP 8.1+) pour gérer l'erreur manuellement
+mysqli_report(MYSQLI_REPORT_OFF);
 
 // Créer la connexion
 $conn = new mysqli($servername, $username, $password, $dbname);
